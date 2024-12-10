ac221cc42bf13673e0b8e9b3115873a229072c8e
Amit <amit.halkati@gmail.com>
2024-12-10 21:37:22 +0530

    Initial commit for HRM project

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 # Base URL and credentials
 BaseUrl = "https://opensource-demo.orangehrmlive.com/"
 Username = "Admin"
